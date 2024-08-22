204c6b2c424cd830a2c024ea693f80cd7896cbdc
williantopac <williantopac@gmail.com>
2024-08-21 23:06:50 -0300

    adicionar condicionais de verificacao de chute

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,23 @@
 alert('Bem vindo ao jogo do numero secreto!');
+let numeroSecreto = 29;
+console.log(numeroSecreto);
+let chute = prompt ("Escolha um numero entre 1 e 30.");
+
+
+//condição que verifica chute.
+
+if (chute == numeroSecreto) {
+    alert(`Parabéns, você descobriu o numero Secreto! ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto) {
+        alert(`O numero secreto é menor que ${chute}`);
+    } else {
+        alert(`O numero secreto é maior que ${chute}`);
+    }
+}
+
+
+
+
+
+
